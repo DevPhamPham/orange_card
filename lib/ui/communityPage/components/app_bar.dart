64926085c4421a5dd5b1b31498f9e6140c017352
DevPhamPham <pham.duykhoa1303@gmail.com>
2024-05-06 22:36:40 +0700

    update commu

--- lib/ui/communityPage/components/app_bar.dart
+++ lib/ui/communityPage/components/app_bar.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:orange_card/app_theme.dart';
 import 'package:orange_card/ui/auth/constants.dart';
 
-
 class CommunityPageAppBar extends StatelessWidget implements PreferredSizeWidget {
   const CommunityPageAppBar({Key? key}) : super(key: key);
 
@@ -10,7 +9,8 @@ class CommunityPageAppBar extends StatelessWidget implements PreferredSizeWidget
   Widget build(BuildContext context) {
     return AppBar(
       title: const Text('Community'),
-      backgroundColor: kPrimaryColor,
+      backgroundColor: Colors.white,
+      centerTitle: true,
       titleTextStyle: AppTheme.title_appbar,
     );
   }
